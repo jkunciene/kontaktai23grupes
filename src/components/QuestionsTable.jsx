965768f50eb5a228b59612dbcc963bf6fb16450f
Mokytojas <Mokytojas@kitm.local>
2023-06-08 11:58:48 +0300

    add ir get metodai veikia

--- src/components/QuestionsTable.jsx
+++ src/components/QuestionsTable.jsx
@@ -1,10 +1,53 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+
+import { db } from '../firebase';
+import { collection, getDocs } from "firebase/firestore";
+
+import Table from 'react-bootstrap/Table';
+
 
 const QuestionsTable = () => {
+    const [questions, setQuestion] = useState([]);
+
+    const getDataFromFirestore = async () => {
+        const data = await getDocs(collection(db, "clientsReq"));
+        const tempQuestion = data.docs.map(doc => (
+            {
+                id: doc.id,
+                ...doc.data()
+            }));
+        // console.log(tempQuestion);
+        setQuestion(tempQuestion);
+    }
+
+    useEffect(() => {
+        //kada iskviesti duomenu gavimo funkcija
+        getDataFromFirestore();
+    }, []);
+
+    console.log(questions);
+
     return (
-        <div>
-            duomenys
-        </div>
+        <Table striped bordered hover size="sm">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Client Name</th>
+                    <th>Question</th>
+                </tr>
+            </thead>
+            <tbody>
+                {questions && questions.map((oneQuestion, index) => (
+                    <tr key={index}>
+                        <td>{index + 1}</td>
+                        <td>{oneQuestion.name}</td>
+                        <td>{oneQuestion.client_message}</td>
+                    </tr>
+                )
+
+                )}
+            </tbody>
+        </Table>
     )
 }
 
